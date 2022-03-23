fb41f93da9adcb503c28b94f89cc31e69ab48b61
tsogp <tsogp@d69+Kg6dQ1VX4zdyiaHaTP4GgUM/+v17f4qLHQhmARQ>
2022-03-23 18:08:57 +0300

    production ready

--- site/setup.py
+++ site/setup.py
@@ -40,6 +40,7 @@ class ProductCategory(db.Model):
     def __repr__(self):
         return f'{self.id} {self.name}'
 
+
 class Product(db.Model):
     id =                db.Column(db.Integer, primary_key=True)
     name =              db.Column(db.String(200), nullable=False)
@@ -55,21 +56,33 @@ class Product(db.Model):
     def __repr__(self):
         return f'{self.id} {self.name}'
 
+    def inc_amount(self, amount):
+        self.amount += amount
+
+    def dec_amount(self, amount):
+        self.amount -= amount
+
 
 class CartItem(db.Model):
     id =                db.Column(db.Integer, primary_key=True)
     user_id =           db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
-    product_id =        db.Column(db.Integer, db.ForeignKey(Product.id), nullable=False)
+    product_name =      db.Column(db.Integer, db.ForeignKey(Product.id), nullable=False)
     price =             db.Column(db.Integer, nullable=False)
     amount =            db.Column(db.Integer, nullable=False)
-    order_id =          db.Column(db.Integer, nullable=False)
     date_created =      db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    is_visible =        db.Column(db.Boolean, default=True)
 
     user = db.relationship('User', backref=db.backref('CartItem', lazy=False))
     product = db.relationship('Product', backref=db.backref('CartItem', lazy=False))
 
     def __repr__(self):
         return f'{self.id}'
+
+    def inc_amount(self, amount):
+        self.amount += amount
+    
+    def update_visibility(self):
+        self.is_visible = False
 
 
 class PurchasedOrder(db.Model):
@@ -115,7 +128,7 @@ def init():
         name='Робуста',
         description='Больше кофеина - хорошой энергетик.\nИнтенсивный аромат.\nТерпкий вкус.\n',
         category_id=1,
-        price=2000, 
+        price=3000, 
         amount=100,
         discount_rate=0,
         url='robusta'
@@ -125,7 +138,7 @@ def init():
         name='Либерика',
         description='Большие зерна.\nОчень (!) сильный аромат.\nОчень (!) резкий вкус.\n',
         category_id=1,
-        price=2000, 
+        price=4000, 
         amount=100,
         discount_rate=0,
         url='liberica'
@@ -143,15 +156,31 @@ def init():
     )
     dummy.set_password('bruh1234')
 
+    dummy_review = Reviews(
+        name='Джаред',
+        email='a@a.ru',
+        text='Отличный кофе. После него я не спал несколько дней.'
+    )
+
+    dummy_review1 = Reviews(
+        name='Джейкоб',
+        email='a@a.ru',
+        text='Согласен с Джаредом! Отличный кофе. После него я тоже не спал несколько дней.'
+    )
+
     db.session.add(dummy)
+    db.session.add(dummy_review)
+    db.session.add(dummy_review1)
     db.session.commit()
 
 def create_db():
     db.create_all()
 
 def get_product_by_url(name):
-    product = list(Product.query.filter(Product.url == name))
-    return product[0] if len(product) == 1 else None
+    return Product.query.filter(Product.url == name).one()
+
+def get_product_id_by_url(name):
+    return Product.query.filter(Product.url == name).one().id
 
 def db_status():
     return os.path.isfile("testdb.db")
@@ -159,48 +188,147 @@ def db_status():
 def get_products():
     return Product.query.all()
 
-def get_reviews():
+def get_reviews():  
     return Reviews.query.all()
 
-def get_cart():
-    return CartItem.query.all()
+def get_cart_for_user(user_id):
+    return CartItem.query.filter(User.id == user_id)
+
+def disable_cart_for_user(user_id):
+    cart = get_cart_for_user(user_id)
+    for cart_item in cart:
+        cart_item.update_visibility()
+        db.session.add(cart_item)
+    db.session.commit()
+
+def get_total_price_for_user(user_id):
+    cart = get_cart_for_user(user_id)
+    total_price = 0
+    for cart_item in cart:
+        if cart_item.is_visible:
+            total_price += cart_item.amount * cart_item.price
+    return total_price
+
+@app.route("/404")
+def render_404():
+    return render_template("404.html")
 
 @app.route('/')
 def setup():
     return redirect('/index')
 
 @app.route('/login', methods=["GET", "POST"])
-def login():
+def render_login():
     if request.method == "POST":
         login = request.form.get("login")
         password = request.form.get("password")
         try:
             if User.query.filter(User.username == login).one().validate(password):
                 session["login"] = login
-                flash(f"Добро пожаловать, {login}!", "success")
                 return redirect('/index', code=301)
             flash("Неправильный пароль", "warning")
         except sqlalchemy.exc.NoResultFound:
-            flash("Неправильный логин", "danger")
+            flash("Неправильный логин", "warning")
     return render_template('login.html')
 
 
 @app.route("/logout")
-def logout():
+def render_logout():
     if session.get("login"):
         session.pop("login")
+        flash("Вы вышли из аккаунта", "success")
     return redirect("/", code=302)
 
 @app.route('/index')
 def render_index():
     return render_template("index.html")
 
+@app.route('/reviews', methods=["GET", "POST"])
+def render_reviews():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        text = request.form.get("review")
+        if name != "" and email != "" and text != "":
+            review = Reviews(name=name,
+                             email=email,
+                             text=text)
+            db.session.add(review)
+            db.session.commit()
+        else:
+            flash('Пожалуйста, заполните все поля', 'warning')
+    return render_template("reviews.html", reviews=get_reviews())
+
+@app.route('/<login>', methods=['GET', 'POST'])
+def render_profile(login):
+    if session.get('login') == login:
+        user = User.query.filter(User.username == login).one()
+        if request.method == 'POST':
+            old_password = request.form.get('old_password')
+            new_password = request.form.get('new_password')
+
+            if old_password:
+                if user.validate(old_password):
+                    user.set_password(new_password)
+                    flash('Пароль изменен', 'success')
+                    db.session.add(user)
+                    db.session.commit()
+                elif new_password == None:
+                    flash('Новый пароль пуст', 'warning')
+                else:
+                    flash('Старый пароль неверен', 'warning')
+        return render_template('profile.html', user=user, cart=get_cart_for_user(user.id), total_price=get_total_price_for_user(user.id))
+        
+    flash('Пожалуйста, войдите в свой аккаунт, чтобы продолжить', 'warning')
+    return redirect('/login', code=301)
+
+@app.route('/buy/<login>', methods=['POST', 'GET'])
+def proceed_payment(login):
+    if session.get('login') == login:
+        user = User.query.filter(User.username == session.get('login')).one()
+        
+        disable_cart_for_user(user.id)
+    
+        flash('Спасибо за покупку!', 'success')   
+
+    return redirect('/catalogue')
+
 @app.route('/catalogue')
 def render_catalogue():
     return render_template("catalogue.html", products=get_products())
 
-@app.route('/<coffee>')
+@app.route('/products/<coffee>', methods=['GET', 'POST'])
 def render_coffee(coffee):
+    if request.method == 'POST':
+        if session.get('login') is not None:
+            user = User.query.filter(User.username==session.get('login')).one()
+            product = get_product_by_url(coffee)
+            if user:
+                quantity = request.form.get('quantity')
+                if get_product_by_url(coffee).amount >= int(quantity):
+                    cartitem = None
+                    try:
+                        cartitem = CartItem.query.filter(sqlalchemy.and_(CartItem.product_name==product.name, CartItem.is_visible==True)).one()
+                        cartitem.inc_amount(int(quantity))
+                    except sqlalchemy.exc.NoResultFound:
+                        cartitem = CartItem(
+                            user_id=user.id,
+                            product_name=product.name,
+                            price=product.price,
+                            amount=quantity
+                        )
+
+                    product.dec_amount(int(quantity))
+                    db.session.add(cartitem)
+                    db.session.commit()
+
+                    flash('Товар добавлен в корзину', 'success')
+                else:
+                    flash('Недостаточно товара на складe', 'warning')
+                    
+            return render_template("coffee.html", coffee=get_product_by_url(coffee))
+
+        flash('Пожалуйста, войдите в свой аккаунт, чтобы продолжить', 'warning')
     return render_template("coffee.html", coffee=get_product_by_url(coffee))
 
 if __name__ == '__main__':
