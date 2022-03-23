ac76beec8c59caa8f9b816cc8fd44e1cf01aeaa5
tsogp <tsogp@d69+Kg6dQ1VX4zdyiaHaTP4GgUM/+v17f4qLHQhmARQ>
2022-03-24 00:09:29 +0300

    nah just kidding v2

--- app/setup.py
+++ app/setup.py
@@ -2,9 +2,7 @@ from flask import *
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testdb.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'content'
+app.config.from_object("config")
 
 db = SQLAlchemy(app)
 
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,4 @@
 from app.setup import app
-from app.models import db_status, create_db, init
 
-if __name__ == '__main__':
-    if not db_status():
-        create_db()
-        init()
-    
+if __name__ == '__main__':    
     app.run(host='0.0.0.0')
